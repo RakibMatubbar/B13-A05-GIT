ea15a012696c9b3424e206097a99b09380fb8dc7
RakibMatubbar <rakibmatubbar996@gmail.com>
2026-03-07 20:20:07 +0600

    Change Clicked btn Color & Remove Other Btns Color & It's Function

--- script/machine.js
+++ script/machine.js
@@ -20,3 +20,36 @@ function showOnly(id) {
 
 
 
+// TO CHANGE HEADER BTN COLORS:
+
+const headerBtnContaienr = document.getElementById("header-btn-container");
+
+// Event Delegation to Add Colors:
+headerBtnContaienr.addEventListener("click", (event) => {
+    const btnContainer = event.target.closest("button");
+
+    if(!btnContainer) {
+        return;
+    }
+
+    // To remove Colors by using for...of:
+    const allButtons = headerBtnContaienr.querySelectorAll("button");
+
+    for(const button of allButtons){
+        button.classList.remove('bg-blue-600', 'bg-green-600', 'bg-purple-600', 'text-white');
+    }
+
+    // Add Colors After Clicking:
+    if(btnContainer.id === "all-btn") {
+        btnContainer.classList.add('bg-blue-600', 'text-white');
+    }
+    else if(btnContainer.id === "open-btn") {
+        btnContainer.classList.add('bg-green-600', 'text-white');
+    }
+    else if(btnContainer.id === "closed-btn") {
+        btnContainer.classList.add('bg-purple-600', 'text-white');
+    }
+
+});
+
+
